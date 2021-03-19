c41ae97efb4ab3e35be5cddf0296c1267bca635b
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-03-19 09:20:18 +0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlGMctnDhFY357JohWvCAsriAmdZmMbdE",
+    authDomain: "assignment-9withauthentication.firebaseapp.com",
+    projectId: "assignment-9withauthentication",
+    storageBucket: "assignment-9withauthentication.appspot.com",
+    messagingSenderId: "975545445947",
+    appId: "1:975545445947:web:6d63649a47d1fccd12b61e"
+  };
+  export default firebaseConfig;
