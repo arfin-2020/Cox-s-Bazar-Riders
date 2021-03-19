77ef2456fa8266fd56ed9bb4b0b6af0e9bf38d30
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-03-19 09:37:02 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
+import Login from './components/Login/Login';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Book from './components/Book/Book';
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+          <Header/>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Book path="/book">
+              <Book />
+            </Book>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
